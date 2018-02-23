fc0a46824741fd69ca1945060e54cf4f13c29f59
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2018-02-23 15:41:22 -0800

    add delete by id method to client class

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -252,5 +252,26 @@ namespace HairSalon.Models
       }
     }
 
+    public void Delete ()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM clients WHERE id = @thisId;";
+
+      MySqlParameter thisId = new MySqlParameter();
+      thisId.ParameterName = "@thisId";
+      thisId.Value = this._id;
+      cmd.Parameters.Add(thisId);
+
+      cmd.ExecuteNonQuery();
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
   }
 }
