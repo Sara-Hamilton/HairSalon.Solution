acb1ce053da736ebaf9b8f2b16d957644f64856d
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2018-02-23 13:19:07 -0800

    add primary get and post methods to clients controller

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -7,6 +7,29 @@ namespace HairSalon.Controllers
 {
   public class ClientsController : Controller
   {
+    [HttpGet("/clients")]
+    public ActionResult Index()
+    {
+      List<Client> allClients = Client.GetAll();
+      return View(allClients);
+    }
+
+    [HttpPost("/clients")]
+    public ActionResult Create()
+    {
+      Client newClient = new Client (Request.Form["new-name"], Request.Form["new-phone"], RequestForm["new-notes"], Int32.Parse(Request.Form["new-stylist"]));
+      newClient.Save();
+      List<Client> allClients = Client.GetAll();
+      return View("Index", allClients);
+    }
+
+    [HttpGet("/clients/new")]
+    public ActionResult CreateForm()
+    {
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View(allStylists);
+    }
+
 
   }
 }
