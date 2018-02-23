47a1d2f59ad66f5c8ab08dbe9e9e0b6ba40a0827
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2018-02-23 13:03:46 -0800

    add green condition for get all empty database at first test method for client class

--- HairSalon.Tests/ModelsTests/ClientTests.cs
+++ HairSalon.Tests/ModelsTests/ClientTests.cs
@@ -25,7 +25,7 @@ namespace HairSalon.Models.Tests
       int result = Client.GetAll().Count;
 
       //Assert
-      Assert.AreEqual(1, result);
+      Assert.AreEqual(0, result);
     }
 
     [TestMethod]
