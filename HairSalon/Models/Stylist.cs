6145f4d00fcacec5d81db6d400d1b8ec3af6a860
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2018-02-23 11:37:07 -0800

    add get all clients method to stylist class

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -183,6 +183,38 @@ namespace HairSalon.Models
       return newStylist;
     }
 
+    public List<Client> GetClients()
+   {
+     List<Client> allClientsByStylist = new List<Client> {};
+     MySqlConnection conn = DB.Connection();
+     conn.Open();
+     var cmd = conn.CreateCommand() as MySqlCommand;
+     cmd.CommandText = @"SELECT * FROM clients WHERE stylist_id = @stylist_id;";
+
+     MySqlParameter stylistId = new MySqlParameter();
+     stylistId.ParameterName = "@stylist_id";
+     stylistId.Value = this._id;
+     cmd.Parameters.Add(stylistId);
+
+     var rdr = cmd.ExecuteReader() as MySqlDataReader;
+     while(rdr.Read())
+     {
+       int clientId = rdr.GetInt32(0);
+       string clientName = rdr.GetString(1);
+       string clientPhone = rdr.GetString(2);
+       string clientNotes = rdr.GetString(3);
+       int clientStylistId = rdr.GetInt32(4);
+       Client newClient = new Client(clientName, clientPhone, clientNotes, clientStylistId, clientId);
+       allClientsByStylist.Add(newClient);
+     }
+     conn.Close();
+     if (conn != null)
+     {
+       conn.Dispose();
+     }
+     return allClientsByStylist;
+   }
+
 
 
   }
