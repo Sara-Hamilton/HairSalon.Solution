c6978915003c89d136cc3f5412ce3d0a55f34c34
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2018-02-23 15:57:26 -0800

    add routes to clients controller for delete and edit methods

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -30,6 +30,54 @@ namespace HairSalon.Controllers
       return View(allStylists);
     }
 
+    [HttpPost("/clients/delete")]
+    public ActionResult DeleteAll()
+    {
+      Client.DeleteAll();
+      List<Client> allClients = Client.GetAll();
+      return View("Index", allClients);
+    }
+
+    [HttpGet("/clients/{id}/update")]
+    public ActionResult UpdateForm(int id)
+    {
+      Client thisClient = Client.Find(id);
+      Stylist thisStylist = Stylist.Find(thisClient.GetStylistId());
+      List<Stylist> allStylists = Stylist.GetAll();
+      Dictionary<string, object> clientDetails = new Dictionary <string, object>();
+      clientDetails.Add("client", thisClient);
+      clientDetails.Add("stylists", allStylists);
+      return View(clientDetails);
+    }
+
+    [HttpPost("/clients/{id}/update")]
+    public ActionResult Update(int id)
+    {
+      Client thisClient = Client.Find(id);
+      thisClient.Edit(Request.Form["new-name"], Request.Form["new-phone"], Request.Form["new-notes"], Int32.Parse(Request.Form["new-stylist"]));
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/clients/{id}/delete")]
+    public ActionResult Delete(int id)
+    {
+      Client thisClient = Client.Find(id);
+      thisClient.Delete();
+      List<Client> allClients = Client.GetAll();
+      return View("Index", allClients);
+    }
+
+    [HttpGet("/clients/{id}/details")]
+    public ActionResult Details(int id)
+    {
+      Client thisClient = Client.Find(id);
+      Stylist thisStylist = Stylist.Find(thisClient.GetStylistId());
+      List<Review> allReviews = thisClient.GetReviews();
+      Dictionary<string, object> clientDetails = new Dictionary <string, object>();
+      clientDetails.Add("client", thisClient);
+      clientDetails.Add("stylist", thisStylist);
+      return View(clientDetails);
+    }
 
   }
 }
