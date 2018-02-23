92f9bbf3daca555e83cc6eaef8df5b279c3ec2ad
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2018-02-23 13:09:59 -0800

    add primary get and post methods to stylists controller

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -7,5 +7,26 @@ namespace HairSalon.Controllers
 {
   public class StylistsController : Controller
   {
+    [HttpGet("/stylists")]
+    public ActionResult Index()
+    {
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View(allStylists);
+    }
+
+    [HttpPost("/stylists")]
+    public ActionResult Create()
+    {
+      Stylist newStylist = new Stylist (Request.Form["new-name"]);
+      newStylist.Save();
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View("Index", allStylists);
+    }
+
+    [HttpGet("/stylists/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
   }
 }
