1acd5f5a1bf449490fa75ffdc99e3ff289e2d92f
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2018-02-23 10:28:52 -0800

    add red condition for setters test method

--- HairSalon.Tests/ModelsTests/StylistTests.cs
+++ HairSalon.Tests/ModelsTests/StylistTests.cs
@@ -38,5 +38,31 @@ namespace HairSalon.Models.Tests
       Assert.AreEqual(phone, phoneResult);
     }
 
+    [TestMethod]
+    public void Setters_TestingAllSetters_Various()
+    {
+      //Arrange
+      string name = "John Smith";
+      string name2 = "Jane Doe";
+      DateTime hireDate = new DateTime (2015, 3, 1);
+      DateTime hireDate2 = new DateTime (2013, 8, 5);
+      string phone = "503-555-5555";
+      string phone2 = "503-555-6789";
+      Stylist newStylist = new Stylist(name, hireDate, phone);
+
+      //Act
+      newStylist.SetName(name2);
+      newStylist.SetHireDate(hireDate2);
+      newStylist.SetPhone(phone2);
+      string nameResult = newStylist.GetName();
+      DateTime hireDateResult = newStylist.GetHireDate();
+      string phoneResult = newStylist.GetPhone();
+
+      //Assert
+      Assert.AreEqual(name, nameResult);
+      Assert.AreEqual(hireDate, hireDateResult);
+      Assert.AreEqual(phone, phoneResult);
+    }
+
   }
 }
