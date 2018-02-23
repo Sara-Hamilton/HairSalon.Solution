b038fa704105c7878f8f4a7cb29760063a2eedd0
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2018-02-23 11:50:50 -0800

    add override equals, override getHashCode, getters, and setters for client class

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -22,5 +22,68 @@ namespace HairSalon.Models
       _stylistId = stylistId;
     }
 
+    public override bool Equals(System.Object otherClient)
+    {
+      if(!(otherClient is Client))
+      {
+        return false;
+      }
+      else
+      {
+        Client newClient = (Client) otherClient;
+        bool idEquality = (this.GetId() == newClient.GetId());
+        bool nameEquality = (this.GetName() == newClient.GetName());
+        bool phoneEquality = (this.GetPhone() == newClient.GetPhone());
+        bool notesEquality = (this.GetNotes() == newClient.GetNotes());
+        bool stylistIdEquality = (this.GetStylistId() == newClient.GetStylistId());
+        return (idEquality && nameEquality && phoneEquality && notesEquality && stylistIdEquality);
+      }
+    }
+
+    public override int GetHashCode()
+    {
+      return this.GetName().GetHashCode();
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public string GetPhone()
+    {
+      return _phone;
+    }
+
+    public void SetPhone(string newPhone)
+    {
+      _phone = newPhone;
+    }
+
+    public string GetNotes()
+    {
+      return _notes;
+    }
+
+    public void SetNotes(string newNotes)
+    {
+      _notes = newNotes;
+    }
+
+    public int GetStylistId()
+    {
+      return _stylistId;
+    }
+
   }
 }
